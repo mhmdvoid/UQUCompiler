677f2680c686e9c72dc75b1fb6ee5af25ed7533a
Mohammed <moody54324@gmail.com>
2021-08-01 19:49:16 +0300

    Handle End of parsing, To sema-check if there's any unresolvedTypes. For now we're supporting types only, In Name Binding we should support unresolved names && values.

--- src/main/java/ast/redesign/ASTInfo.java
+++ src/main/java/ast/redesign/ASTInfo.java
@@ -15,12 +15,14 @@ public class ASTInfo {
     // id table;
     public Type int32Type;
     public Type unresolvedType;
+    public Type bool8Type;
 
     Set<String> table;
 
     public ASTInfo() {
         int32Type = new Builtin(TypeKind.BUILTIN_32INT_KIND, "int");
         unresolvedType = new UnresolvedType(TypeKind.UNRESOLVED_KIND, ""); // Later.
+        bool8Type = new Builtin(TypeKind.BOOL_8_KIND, "bool");
         table = new HashSet<>();
     }
 
--- src/main/java/ast/redesign/Identifier.java
+++ src/main/java/ast/redesign/Identifier.java
@@ -1,5 +1,7 @@
 package ast.redesign;
 
+import java.util.Objects;
+
 public class Identifier {
     public String name;
 
@@ -13,5 +15,18 @@ public class Identifier {
         return "Identifier{" +
                 "name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Identifier that = (Identifier) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
